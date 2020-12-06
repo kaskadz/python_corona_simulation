02be1626acf95c5df9ac9f916af85442977b4016
Kasper <kasper.kadzielawa@gmail.com>
2020-12-06 13:02:12 +0100

    Implement sobol sensitivity analysis

--- sensitivity.py
+++ sensitivity.py
@@ -4,12 +4,14 @@ from SALib.test_functions import Ishigami
 from simulation import Simulation
 import numpy as np
 import pandas as pd
-
+from tqdm import tqdm
+import warnings
+warnings.filterwarnings("ignore")
 
 variables = [
     ('mean_age', 30, 50),
     ('risk_age', 40, 55),
-    ('critical_mortality_chance', 0.05, 0.2)
+    # ('critical_mortality_chance', 0.05, 0.2)
 ]
 
 result_variables = [
@@ -31,36 +33,36 @@ problem = {
 
 
 if __name__ == '__main__':
-    sim = Simulation(pop_size = 500)
+    print('Generating parameter sets')
+    param_values = saltelli.sample(problem, 10, calc_second_order=False)
 
-    sim.Config.quiet = True
-    sim.Config.visualise = False
-    sim.Config.verbose = False
-    sim.Config.simulation_steps = 500
-    sim.Config.print_summary = False
-    res = sim.run()
-    print(res)
+    inputs = [dict(zip(labels, v)) for v in param_values]
 
-    # param_values = saltelli.sample(problem, 10, calc_second_order=True)
+    results = []
+    for i in tqdm(iterable=inputs, unit='simulation', desc='Running simulations'):
+        sim = Simulation(kwargs=i)
 
-    # inputs = [dict(zip(labels, v)) for v in param_values]
-    # results = []
+        sim.Config.quiet = True
+        sim.Config.visualise = False
+        sim.Config.verbose = False
+        sim.Config.simulation_steps = 500
+        sim.Config.print_summary = False
 
-    # for i in inputs:
-    #     sim = Simulation(kwargs=i)
+        result = sim.run()
+        result_dict = {f'r_{k}': v for k, v in result.items()}
+        param_dict = {f'p_{k}': v for k, v in i.items()}
 
-    #     sim.Config.visualise = False
-    #     sim.Config.verbose = False
-    #     sim.Config.simulation_steps = 500
-    #     sim.Config.print_summary = False
+        results.append({**param_dict, **result_dict})
 
-    #     result = sim.run()
+    df = pd.DataFrame(data = results)
+    print('Saving simulations results')
+    df.to_csv('results.csv', index=False)
 
-    # df = pd.DataFrame(data = results)
-    # df.to_csv('results.csv')
-
-    # for rv in result_variables:
-    #     Y = df[rv].values
-    #     Si = sobol.analyze(problem, Y, print_to_console=True)
-    #     sdf = Si.to_df()
-    #     sdf.to_csv(f'{rv}_results.csv')
+    df = pd.read_csv('results.csv')
+    for rv in tqdm(iterable=result_variables, unit='variable', desc='Computing sensitivity'):
+        Y = df[f'r_{rv}'].values
+        Si = sobol.analyze(problem, Y, calc_second_order=False)
+        sdf = pd.concat(Si.to_df(), axis=1)
+        sdf.index.name = 'parameter'
+        sdf.to_csv(f'{rv}_results.csv')
+    print('Done')
